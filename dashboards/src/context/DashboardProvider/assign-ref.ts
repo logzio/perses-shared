74eaaf3a3d8e01b30eb71472ce2b10fb28c23982
OrReuben <orreuven1243@gmail.com>
2026-02-19 10:25:57 +0200

    License
    Signed-off-by: OrReuben <orreuven1243@gmail.com>

--- dashboards/src/context/DashboardProvider/assign-ref.ts
+++ dashboards/src/context/DashboardProvider/assign-ref.ts
@@ -1,5 +1,3 @@
-// LOGZ.IO FILE
-
 // Copyright The Perses Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,6 +10,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// LOGZ.IO FILE
 
 export type MutableRef<T> = React.RefCallback<T> | React.MutableRefObject<T | null> | null;
 
